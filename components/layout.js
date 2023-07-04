ad17aaef59dc477ca61554ed8221d564d27d9c7e
Caesura Media Net <andyc@caesuramedia.net>
2023-07-04 20:28:34 +0100

    Tidy up from template.

--- components/layout.js
+++ components/layout.js
@@ -31,25 +31,25 @@ export default function Layout({ children, home }) {
 
       <title>AKJ Cragg | Lens Based Art</title>
 
-      <link rel="apple-touch-icon"       href="/img/memtyles-logo.png" sizes="180x180" />
-      <link rel="icon"                   href="/img/memtyles-logo.png" sizes="32x32" type="image/png" />
-      <link rel="icon"                   href="/img/memtyles-logo.png" sizes="16x16" type="image/png" />
-      <link rel="mask-icon"              href="/img/memtyles-logo.png" color="#563d7c" />
-      <link rel="icon"                   href="/img/memtyles-logo.png" />
+      <link rel="apple-touch-icon"       href="/img/akjcragg-logo.png" sizes="180x180" />
+      <link rel="icon"                   href="/img/akjcragg-logo.png" sizes="32x32" type="image/png" />
+      <link rel="icon"                   href="/img/akjcragg-logo.png" sizes="16x16" type="image/png" />
+      <link rel="mask-icon"              href="/img/akjcragg-logo.png" color="#563d7c" />
+      <link rel="icon"                   href="/img/akjcragg-logo.png" />
 
       <meta name="twitter:card"         content="summary" />
       <meta name="twitter:site"         content="@andykjcragg" />
       <meta name="twitter:creator"      content="@andykjcragg" />
       <meta name="twitter:title"        content="AKJ Cragg Lens-Based Art" />
       <meta name="twitter:description"  content="AKJ Cragg Lens-Based Art" />
-      <meta name="twitter:image"        content="https://memtyles.vercel.app/img/memtyles-logo.png" />
+      <meta name="twitter:image"        content="https://akjcragg.vercel.app/img/akjcragg-logo.png" />
 
       <meta property="og:url"              content="https://akjcragg.com" />
       <meta property="og:title"            content="AKJ Cragg Lens-Based Art" />
       <meta property="og:description"      content="AKJ Cragg Lens-Based Art" />
       <meta property="og:type"             content="website" />
-      <meta property="og:image"            content="https://akjcragg.com/img/memtyles-logo.png" />
-      <meta property="og:image:secure_url" content="https://akjcragg.com/img/memtyles-logo.png" />
+      <meta property="og:image"            content="https://akjcragg.com/img/akjcragg-logo.png" />
+      <meta property="og:image:secure_url" content="https://akjcragg.com/img/akjcragg-logo.png" />
       <meta property="og:image:type"       content="image/png" />
       <meta property="og:image:width"      content="50" />
       <meta property="og:image:height"     content="50" />
