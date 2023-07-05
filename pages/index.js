f6026e00409c2caf235c96fbf030f501684eb6b3
Caesura Media Net <andyc@caesuramedia.net>
2023-07-05 12:48:52 +0100

    Basic Masonry with MUI

--- pages/index.js
+++ pages/index.js
@@ -27,11 +27,51 @@ import {
    faPuzzlePiece,
 } from '@fortawesome/free-solid-svg-icons'
 
+// MUI Material UI
+//
+import Box from '@mui/material/Box';
+import Masonry from '@mui/lab/Masonry';
+
+// Local components.
+//
+
+// Constants.
+//
+import {ImageList} from '../constants';
+
+
 export default function AKJCragg () {
    return (
       <Layout> {/* A Next.js idea */}
          <Container fluid> 
             <h1>AKJ Cragg</h1>
+            <Masonry columns={4} spacing={5}>
+               {ImageList.map((item, index) => (
+                  <div key={index}
+                     style={{
+                        borderRadius: '0.2rem',
+                        overflow:'hidden',
+                        border: '1px solid rgba(0, 0, 0, 1.0)',
+                        padding:5,
+                        boxShadow: '0px 0px 15px 5px #D2D6C5',
+                     }}
+                  >
+                     <div>
+                        {item.replace(/^.img.(.+).jpg/, '$1')}
+                     </div>
+                     <img
+                        src={`${item}?w=162&auto=format`}
+                        srcSet={`${item}?w=162&auto=format&dpr=2 2x`}
+                        alt={item}
+                        loading="lazy"
+                        style={{
+                           display: 'block',
+                           width: '100%',
+                        }}
+                     />
+                  </div>
+               ))}
+            </Masonry>
          </Container>
       </Layout>
    );
