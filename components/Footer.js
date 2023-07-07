2aeef70def1c744c3dad72c32aed11a647ee7a73
Caesura Media Net <andyc@caesuramedia.net>
2023-07-07 17:53:33 +0100

    Styling other site section.

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,13 @@
-import Link from 'next/link';
-import styles from '../styles/akjcragg.module.css';
+import Link                      from 'next/link';
+import styles                    from '../styles/akjcragg.module.css';
 import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+import Grid                      from '@mui/material/Unstable_Grid2';
 
-const iconWidth = 45;
+const iconWidth = 25;
+const aStyles = {
+   color          : 'unset',
+   textDecoration : 'underline',
+};
 
 export default function Footer () {
    return (
@@ -13,38 +18,50 @@ export default function Footer () {
          <p>
             <Link style={{textDecoration: "underline", color : 'unset'}} href="/privacypolicy">Privacy Policy</Link>
          </p>
-         <div style={{
-            borderTop      : "1px solid black",
-            marginLeft     : '20vw',
-            marginRight    : '20vw',
-            flexDirection  : 'row',
-            display        : "flex",
-            justifyContent : 'space-around',
-            alignItems     : 'center'}}
-         >
-            <a href="https://chintzbaby.com" target="_blank">
-               <img src="/img/chintzbaby-cat.png" width={iconWidth} />
-            </a>
-            <a href="https://andycragg.com/art" target="_blank">
-               <img src="/img/acart-logo.png" width={iconWidth} />
-            </a>
-            <a href="https://andycragg.com" target="_blank">
-               <img src="/img/akjc-logo.png" width={iconWidth} />
-            </a>
-            <a href="https://andykjcragg.co.uk" target="_blank">
-               <img src="/img/andykjcragg-logo-large.png" width={iconWidth} />
-            </a>
-            <a href="https://scrembl.com" target="_blank">
-               <img src="/img/scremblv2-icon.png" width={iconWidth} />
-            </a>
-            <a href="https://memtyles.com" target="_blank">
-               <img src="/img/memtyles-logo.svg" width={iconWidth} />
-            </a>
-         </div>
+         <p style={{width : '100%',borderTop : "1px solid black"}}></p>
+         <p  style={{fontSize : 16}}>My other sites</p>
+         <Grid container spacing={2}>
+            <Grid md={2}>
+               <a style={aStyles} href="https://chintzbaby.com" target="_blank">
+                  <img src="/img/chintzbaby-cat.png" width={iconWidth} />
+                  &nbsp;ChintzBaby
+               </a>
+            </Grid>
+            <Grid  md={2}>
+               <a style={aStyles} href="https://andycragg.com/art" target="_blank">
+                  <img src="/img/acart-logo.png" width={iconWidth} />
+                  &nbsp;Art
+               </a>
+            </Grid>
+            <Grid md={2}>
+               <a style={aStyles} href="https://andycragg.com" target="_blank">
+                  <img src="/img/akjc-logo.png" width={iconWidth} />
+                  &nbsp;All sites
+               </a>
+            </Grid>
+            <Grid md={2}>
+               <a style={aStyles} href="https://andykjcragg.co.uk" target="_blank">
+                  <img src="/img/andykjcragg-logo-large.png" width={iconWidth} />
+                  &nbsp;Stories
+               </a>
+            </Grid>
+            <Grid md={2}>
+               <a style={aStyles} href="https://scrembl.com" target="_blank">
+                  <img src="/img/scremblv2-icon.png" width={iconWidth} />
+                  &nbsp;Message Obfustication
+               </a>
+            </Grid>
+            <Grid md={2}>
+               <a style={aStyles} href="https://memtyles.com" target="_blank">
+                  <img src="/img/memtyles-logo.svg" width={iconWidth} />
+                  &nbsp;Memory Game
+               </a>
+            </Grid>
+         </Grid>
          <div
             onClick={() => window.scrollTo({top:0, behavior : 'smooth'})}
             style={{position : 'fixed', bottom : 5, right : 2}}>
-            <KeyboardDoubleArrowUpIcon color="secondary" sx={{ fontSize: iconWidth }} />
+            <KeyboardDoubleArrowUpIcon color="secondary" sx={{ fontSize: 45 }} />
          </div>
       </footer>
    );
