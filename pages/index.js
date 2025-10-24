524ee34c399a0fab0c9b5363540b39e25aa45027
Caesura Media Net <andyc@caesuramedia.net>
2025-10-24 10:37:32 +0100

    Addes Staging text

--- pages/index.js
+++ pages/index.js
@@ -134,7 +134,7 @@ export default function AKJCragg () {
    return (
       <Layout>
          <div style={{flexDirection : 'row', display: "flex", justifyContent : 'space-between', alignItems : 'center'}}>
-            <h1 style={{textAlign: 'left', fontSize : h1FontSize}}>AKJ Cragg | Lens</h1>
+            <h1 style={{textAlign: 'left', fontSize : h1FontSize}}>AKJ Cragg | Lens | Staging</h1>
             <Link style={{fontSize : h1FontSize/2, textDecoration: "underline", color : 'unset'}} href="/about">About</Link>
          </div>
          <div style={{textAlign : 'left'}}>
